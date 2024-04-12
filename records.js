8076e5ffd47a96acb71c4f6212ed1538021a8f70
Josiewsc <160953964+Josiewsc@users.noreply.github.com>
2024-04-13 11:02:26 +1200

    yes

---
+++ records.js
@@ -0,0 +1,26 @@
+const answers = {
+    colour: '',
+    fruit: '',
+    season: '',
+    food: '',
+    dessert: '',
+    hair: '',
+    singer: '',
+    country: '',
+    princess: '',
+}
+
+const superpowerTally = {
+    shapeShifting: 0,
+    mindReading: 0,
+    superStrength: 0,
+    teleportation: 0,
+}
+
+
+const updateAnswer = (answerName, answerValue, superpower) => {
+    answers[answerName] = answerValue;
+    superpowerTally[superpower] = superpowerTally[superpower] + 1;
+    console.log('answers', answers)
+    console.log('superpower', superpower, 'tally', superpowerTally[superpower])
+}
