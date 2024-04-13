3c5c1b10628b243b6c7c9152a46e36d0a6b599cb
Josiewsc <160953964+Josiewsc@users.noreply.github.com>
2024-04-13 13:30:35 +1200

    commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const resetLocalStorageValues = () => {
+    localStorage.setItem('shapeShifting', 0);
+    localStorage.setItem('mindReading', 0);
+    localStorage.setItem('superStrength', 0);
+    localStorage.setItem('teleportation', 0);
+}
+
+resetLocalStorageValues();
--- records.js
+++ records.js
@@ -1,26 +1,37 @@
-window.answers = {
-    colour: '',
-    fruit: '',
-    season: '',
-    food: '',
-    dessert: '',
-    hair: '',
-    singer: '',
-    country: '',
-    princess: '',
+const updateAnswer = (superpower) => {
+    const superpowerTally = localStorage.getItem(superpower)
+    const newValue = Number(superpowerTally) + 1
+    localStorage.setItem(superpower, newValue);
 }
 
-window.superpowerTally = {
-    shapeShifting: 0,
-    mindReading: 0,
-    superStrength: 0,
-    teleportation: 0,
-}
+const findUsersPower = (superpower) => {
+    updateAnswer(superpower);
+    const superPowers = {
+        shapeShifting: localStorage.getItem('shapeShifting'),
+        mindReading:  localStorage.getItem('mindReading'),
+        superStrength : localStorage.getItem('superStrength'),
+        teleportation :localStorage.getItem('teleportation'),
+    }
 
+    const yourSuperpower = Object.keys(superPowers)
+                     .reduce(function(keya, keyb) {
+                         return superPowers[keya] > superPowers[keyb] ? keya : keyb;
+                     });
 
-const updateAnswer = (answerName, answerValue, superpower) => {
-    answers[answerName] = answerValue;
-    superpowerTally[superpower] = superpowerTally[superpower] + 1;
-    console.log('answers', answers)
-    console.log('superpower', superpower, 'tally', superpowerTally[superpower])
+     if(yourSuperpower === 'shapeShifting') {
+
+        window.location.href = 'http://127.0.0.1:5500/shapeshifting.html'
+     }      
+     
+     if(yourSuperpower === 'superStrength') {
+        window.location.href = 'http://127.0.0.1:5500/super-strength.html'
+     }   
+
+     if(yourSuperpower === 'teleportation') {
+        window.location.href = 'http://127.0.0.1:5500/teleportation.html'
+     }   
+
+     if(yourSuperpower === 'mindReading') {
+        window.location.href = 'http://127.0.0.1:5500/mind-reading.html'
+     }   
 }
